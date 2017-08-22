1f81e5697e2d922d1cc64b583bed2c2782d40437
EMichira <emmanuelmichira@gmail.com>
2017-08-22 15:23:25 +0300

    Added password to DB

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,8 +8,19 @@ public class App {
    staticFileLocation("/public");
    String layout = "templates/layout.vtl";
 
+   ProcessBuilder process = new ProcessBuilder();
+   Integer port;
+   if (process.environment().get("PORT") != null) {
+       port = Integer.parseInt(process.environment().get("PORT"));
+   } else {
+       port = 4567;
+   }
+
+  setPort(port);
+
    get("/", (request, response) -> {
      Map<String, Object> model = new HashMap<String, Object>();
+     model.put("categories", Category.all());
      model.put("template", "templates/index.vtl");
      return new ModelAndView(model, layout);
    }, new VelocityTemplateEngine());
@@ -97,6 +108,6 @@ public class App {
      model.put("template", "templates/category-tasks-success.vtl");
      return new ModelAndView(model, layout);
    }, new VelocityTemplateEngine());
-   
+
  }
 }
